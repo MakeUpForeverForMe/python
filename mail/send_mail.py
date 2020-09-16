935a1285fd40b61f07604665948b6c390a8b7be6
魏喜明 <664651151@qq.com>
2020-09-17 02:31:36 +0800

    2020-09-17 提交发送邮件

---
+++ mail/send_mail.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+import sys
+from email.mime.text import MIMEText
+from email.header import Header
+from configparser import ConfigParser
+
+""" 获取参数 """
+if len(sys.argv) - 1 != 3:
+    print("Error：指定配置文件、消息内容、主题", sys.argv)
+    sys.exit(1)
+
+config_filePath = sys.argv[1]
+email_subject = sys.argv[2]
+message = sys.argv[3]
+
+send_host = '10.80.0.133'
+send_user = 'DataCenter-Alert@services.weshreholdings.com'
+receivers = 'chao.guo@weshareholdings.com', \
+            'jian.tan@weshareholdings.com', \
+            'yunan.huang@weshareholdings.com', \
+            'huan.liu@weshareholdings.com', \
+            'yuheng.wang@weshareholdings.com', \
+            'ximing.wei@weshareholdings.com'
+
+""" 解析配置文件 """
+config = ConfigParser(allow_no_value=True)  # 创建对象
+if len(config.read(config_filePath, encoding='utf-8')) == 0:  # 读取配置文件，如果配置文件不存在则创建
+    print(f"Error：没有找到对应的配置文件 {config_filePath}")
+    sys.exit(1)
+
+if not config.has_section('sender'):  # 检查指定节点是否存在，返回True或False
+    config.add_section('sender')
+    config.set('sender', 'host', send_host)
+    config.set('sender', 'user', send_user)
+
+if not config.has_section('receiver'):
+    print("Error：请在配置文件中配置节点 [receiver]")
+    sys.exit(1)
+
+if not config.has_option('receiver', 'receivers'):
+    print("Error：请在配置文件中配置 receiver 节点的属性 receivers")
+    sys.exit(1)
+
+send_user = config.get('sender', 'user')
+receivers_ = str(config.get('receiver', 'receivers'))
+
+if receivers_ != '':
+    receivers = tuple(receivers_.split(','))
+
+msg = MIMEText(message, 'plain', 'utf-8')  # 邮件内容
+msg['From'] = Header(send_user)
+msg['To'] = Header(','.join(receivers))
+msg['Subject'] = Header('{}'.format(email_subject), 'utf-8')  # 邮件的主题，也可以说是标题
+
+try:
+    # server = smtplib.SMTP(config.get('sender', 'host'), 25)  # 25 为 SMTP 端口号
+    server = smtplib.SMTP_SSL(config.get('sender', 'host'), 465)
+    server.login(send_user, config.get('sender', 'pass'))
+    server.sendmail(send_user, receivers, msg.as_string())  # 发件人、收件人、消息
+    server.quit()  # 退出对话
+    server.close()  # 关闭连接
+except smtplib.SMTPException:
+    print("Error: 无法发送邮件")
---
+++ mail/send_mail_learning.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from configparser import ConfigParser
+
+config = ConfigParser(allow_no_value=True)  # 创建对象
+
+config.read('user.ini', encoding='utf-8')  # 读取配置文件，如果配置文件不存在则创建
+secs = config.sections()  # 获取所有的节点名称
+print(secs)  # ['section1', 'section2']
+
+options = config.options('section1')  # 获取指定节点的所有 key
+print(options)  # ['name', 'age']
+
+item_list = config.items('section1')  # 获取指定节点的键值对
+print(item_list)  # [('name', 'wang'), ('age', '18')]
+
+val = config.get('section1', 'name')  # 获取节点 section1 的 name 属性
+print(val)  # wang
+
+val = config.getint('section1', 'age')  # 获取节点 section1 的 age 属性，属性需要是int型，否则ValueError
+print(val)  # 18
+
+val = config.has_section('section1')  # 检查指定节点是否存在，返回True或False
+print(val)  # True
+
+val = config.has_option('section1', 'age')  # 检查指定节点中是否存在某个key，返回True或False
+print(val)  # True
+
+# 增删改
+# config.add_section("node")  # 添加一个节点，节点名为node, 此时添加的节点node尚未写入文件
+# config.write(open('user.ini', "w"))  # 将添加的节点node写入配置文件
+
+# config.remove_section("node")  # 删除一个节点，节点名为node, 删掉了内存中的节点node
+# config.write(open("user.ini", "w"))  # 将删除节点node后的文件内容回写到配置文件
+
+config.set("node", "k1", "v1")  # 在已存在的节点中添加一个键值对k1 = v1 ，如果key已经存在，则修改value
+# 如果该节点不存在则报错  # configparser.NoSectionError: No section: 'section'
+
+config.write(open("user.ini", "w"))
